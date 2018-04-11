fd991c1005f58798ada2e7834dc0ae0d7c18f777
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-04-11 11:41:54 +0200

    Commit before creating new branch

--- main.js
+++ main.js
@@ -3,13 +3,10 @@ function fetchAllEmploymentAds() {
    		fetch('http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=20&yrkesomradeid=3&lanid=1')
         .then((response) => response.json())
         .then((data) => {
-          let listings = data.matchningslista;
-
+        	let listings = data.matchningslista;
         })
 
-
 }
-
 
 
 let listings2 = fetchAllEmploymentAds();
