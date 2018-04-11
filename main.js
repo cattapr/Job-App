5cd0d9bc2a5128f11d53f47dec8c508e395d8389
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-04-11 12:05:28 +0200

    Sort into module pattern and log response object

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
+//function fetchAllEmploymentAds() {
+//   		return fetch(`http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=20&yrkesomradeid=3&lanid=1`)
+//        .then((response) => response.json())
+//        .then((data) => {
+//        	let listings = data.matchningslista;
+//        })
+//}
 
-function fetchAllEmploymentAds() {
-   		fetch('http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=20&yrkesomradeid=3&lanid=1')
+//let listings2 = fetchAllEmploymentAds();
+//console.log(listings2);
+
+
+const FetchModel = {
+	fetchAll(){
+		 return fetch(`http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=20&yrkesomradeid=3&lanid=1`)
         .then((response) => response.json())
         .then((data) => {
-        	let listings = data.matchningslista;
+        	//let listings = data.matchningslista;
+			 ResponseController.sortResponse(data);
         })
-
+		.catch(error => console.log(error));
+	}
 }
 
+const ResponseController = {
+	sortResponse(data){
+		console.log(data);
+	}
+}
 
-let listings2 = fetchAllEmploymentAds();
-console.log(listings2);
+const View = {
+	output: document.getElementById('output')
+}
+
+FetchModel.fetchAll();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
