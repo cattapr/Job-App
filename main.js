d53dc672ea137045f536e05b71fbc78da49765a8
unknown <catalinapradilla@hotmail.com>
2018-04-11 11:20:43 +0200

    fetch all employments adds

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 
 function fetchAllEmploymentAds() {
-    fetch('http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=20&yrkesomradeid=3&lanid=1')
+   		fetch('http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?sida=1&antalrader=20&yrkesomradeid=3&lanid=1')
         .then((response) => response.json())
-        .then((jobs) => {
-            console.log(jobs);
+        .then((data) => {
+          let listings = data.matchningslista;
+
         })
+
+
 }
 
-fetchAllEmploymentAds();
+
+
+let listings2 = fetchAllEmploymentAds();
+console.log(listings2);
